532628e4a7e0a38cd35d72ed38de592937472845
pletter <petter.rylen@gmail.com>
2025-02-01 23:21:15 +0100

    message fix

--- bokningssystem/backend/src/swishRoutes.js
+++ bokningssystem/backend/src/swishRoutes.js
@@ -47,10 +47,10 @@ router.post('/create-payment', async (req, res) => {
         const paymentData = {
             payeePaymentReference: bookingNumber,
             callbackUrl: `https://aventyrsupplevelsergithubio-testing.up.railway.app/api/swish/swish-callback`,
-            payeeAlias: '1231049352',
+            payeeAlias: '1231049352',,
             currency: 'SEK',
             amount: amount.toString(),
-            message: `Booking ${bookingNumber}`
+            message: `${bookingNumber}`
         };
 
         if (payerAlias) {
